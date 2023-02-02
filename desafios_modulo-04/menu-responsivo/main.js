08c0f68e882df8a1a33feff39fdc6eb2ca818fac
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-02-02 17:38:51 -0300

    Configurando o Media Query  para outros dispositivos

--- desafios_modulo-04/menu-responsivo/main.js
+++ desafios_modulo-04/menu-responsivo/main.js
@@ -7,3 +7,15 @@ function expandirMenu() {
         menu.style.display = 'block'; // No CSS o display está 'none'
     }
 }
+
+
+// Essa função serve para o menu não ficar oculto, caso o usuário diminua e aumente o tamanho da tela.
+function mudouTamanho() {
+    var itensMenu = document.getElementById("menu");
+
+    if (window.innerWidth >= 768) { 
+        itensMenu.style.display = "block";
+    } else {
+        itensMenu.style.display = "none";
+    }
+}
